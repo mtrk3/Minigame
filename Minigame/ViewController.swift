246c7d433f4af96dccdf384083c323658a70dad3
奥野美波 <m_trk3@okunominami-no-MacBook.local>
2015-07-13 13:41:44 +0900

    debug

--- Minigame/ViewController.swift
+++ Minigame/ViewController.swift
@@ -25,18 +25,33 @@ class ViewController: UIViewController {
     }
     @IBAction func generate(){
         for (var i = 0; i < 4; i++) {
-            combination[i] = arc4random_uniform(4)
+            combination[i] = Int(arc4random_uniform(4))
         } //配列のそれぞれの要素に１〜４のランダムの数字を代入
-        label.text = Int(combination[]) //配列をlabelに表示
+        var l1 : Int = combination[0]
+        var l2 : Int = combination[1]
+        var l3 : Int = combination[2]
+        var l4 : Int = combination[3]
+        
+        if combination.count == 4{
+            var label1 : Int = l1 * 1000 + l2 * 100 + l3 * 10 + l4
+            label.text = String(label1)
+        }else if combination.count == 3{
+            var label2 : Int = l1 * 100 + l2 * 10 + l3
+            label.text = String(label2)
+        }else if combination.count == 2{
+            var label3 : Int = l1 * 10 + l2
+            label.text = String(label3)
+        }else if combination.count == 1{
+            var label4 : Int = l1
+            label.text = String(label4)
+        }
+        //combination配列の要素数が４なら４つ、３なら３つ、２なら２つ、１なら１つ、labelに表示されるようにする。
     }
     
     @IBAction func one(){
         var number1 = 1
-        if number1 == combination[0]{
+        if number1 == combination[0] {
             combination.removeAtIndex(0)
-            label.text =
-        }else{
-            label.text = combination
         }
     }
 
@@ -45,9 +60,6 @@ class ViewController: UIViewController {
         var number2 = 2
         if number2 == combination[0]{
             combination.removeAtIndex(0)
-            label.text = Int(combination)
-        }else{
-            label.text = Int(combination)
         }
     }
 
@@ -55,9 +67,6 @@ class ViewController: UIViewController {
         var number3 = 3
         if number3 == combination[0]{
             combination.removeAtIndex(0)
-            label.text = Int(combination)
-        }else{
-            label.text = Int(combination)
         }
     }
 
@@ -65,9 +74,6 @@ class ViewController: UIViewController {
         var number4 = 4
         if number4 == combination[0]{
             combination.removeAtIndex(0)
-            label.text = Int(combination)
-        }else{
-            label.text = Int(combination)
         }
     }
 
